e9d17507d95a87a5e77534efec611385a7802333
Your Name <mine_wongteeravit@hotmail.com>
2018-01-29 21:21:01 +0700

    create main2

---
+++ app/src/main/java/com/custu/project/walktogether/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.custu.project.walktogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.walktogether.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
