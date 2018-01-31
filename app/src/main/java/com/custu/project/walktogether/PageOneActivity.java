ccde3d898c84c6ca613d1b79b650fbeef8ce4db6
Your Name <mine_wongteeravit@hotmail.com>
2018-01-31 16:19:26 +0700

    add survey ui

---
+++ app/src/main/java/com/custu/project/walktogether/PageOneActivity.java
@@ -0,0 +1,28 @@
+package com.custu.project.walktogether;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.AdapterView;
+
+import com.project.walktogether.R;
+
+public class PageOneActivity extends Activity implements AdapterView.OnItemClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().requestFeature(Window.FEATURE_ACTION_BAR);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
+        setContentView(R.layout.page_one);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+    }
+
+
+}
---
+++ app/src/main/java/com/custu/project/walktogether/TopicsOneActivity.java
@@ -0,0 +1,15 @@
+package com.custu.project.walktogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.project.walktogether.R;
+
+public class TopicsOneActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.topics_one);
+    }
+}
