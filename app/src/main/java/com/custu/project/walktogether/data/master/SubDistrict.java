0b4279a900926347423433ac4f3974d586a8cca8
Pannawat Nokket <nogket@gmail.com>
2018-02-02 04:17:32 +0700

    create caretaker model

---
+++ app/src/main/java/com/custu/project/walktogether/data/master/SubDistrict.java
@@ -0,0 +1,36 @@
+package com.custu.project.walktogether.data.master;
+
+/**
+ * Created by pannawatnokket on 2/2/2018 AD.
+ */
+
+public class SubDistrict {
+
+    private Long id;
+    private String name;
+    private String zipCode;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+}
