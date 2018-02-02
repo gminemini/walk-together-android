ca1d06ca3f2733670ddce897b8ac511d5075728f
Pannawat Nokket <nogket@gmail.com>
2018-02-02 16:35:53 +0700

    wip

---
+++ app/src/main/java/com/custu/project/walktogether/util/BasicActivity.java
@@ -0,0 +1,12 @@
+package com.custu.project.walktogether.util;
+
+/**
+ * Created by pannawatnokket on 2/2/2018 AD.
+ */
+
+public interface BasicActivity {
+
+    void setUI() ;
+    void getData();
+    void initProgressDialog();
+}
