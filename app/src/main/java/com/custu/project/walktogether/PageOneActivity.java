c67cf0bea61946235d0388a9ef2081fea63a5d0b
Your Name <mine_wongteeravit@hotmail.com>
2018-02-02 16:40:21 +0700

    question

--- app/src/main/java/com/custu/project/walktogether/PageOneActivity.java
+++ app/src/main/java/com/custu/project/walktogether/PageOneActivity.java
@@ -2,27 +2,12 @@ package com.custu.project.walktogether;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.AdapterView;
 
 import com.custu.project.project.walktogether.R;
-import com.custu.project.walktogether.data.Caretaker;
-import com.custu.project.walktogether.data.Evaluation.Tmse;
-import com.custu.project.walktogether.manager.ConnectServer;
-import com.custu.project.walktogether.model.CaretakerModel;
-import com.custu.project.walktogether.model.EvaluationModel;
-import com.custu.project.walktogether.network.callback.OnDataSuccessListener;
-import com.custu.project.walktogether.util.ConfigService;
-import com.google.gson.JsonObject;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import okhttp3.ResponseBody;
-import retrofit2.Retrofit;
 
 public class PageOneActivity extends Activity implements AdapterView.OnItemClickListener {
 
@@ -33,28 +18,6 @@ public class PageOneActivity extends Activity implements AdapterView.OnItemClick
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SECURE);
         setContentView(R.layout.page_one);
 
-        ConnectServer.getInstance().get(new OnDataSuccessListener() {
-            @Override
-            public void onResponse(JsonObject object, Retrofit retrofit) {
-                List<Caretaker> caretaker = CaretakerModel.getInstance().getListCaretaker(object);
-                Log.d("onResponse", "onResponse: "+caretaker);
-            }
-
-            @Override
-            public void onBodyError(ResponseBody responseBodyError) {
-
-            }
-
-            @Override
-            public void onBodyErrorIsNull() {
-
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-
-            }
-        }, ConfigService.MATCHING+ConfigService.MATCHING_CARETAKER_UNDER_PATIENT+36);
     }
 
     @Override
---
+++ app/src/main/java/com/custu/project/walktogether/QuestionOneActivity.java
@@ -0,0 +1,44 @@
+package com.custu.project.walktogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.custu.project.project.walktogether.R;
+
+import java.util.ArrayList;
+
+public class QuestionOneActivity extends AppCompatActivity {
+    private Spinner answerSpinner;
+    private ArrayList<String> answerArray = new ArrayList<String>();
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.question_one);
+
+
+        setUI();
+        createSpinnerData();
+        ArrayAdapter<String> adapterArray = new ArrayAdapter<String>(this,android.R.layout.simple_dropdown_item_1line, answerArray);
+        answerSpinner.setAdapter(adapterArray);
+    }
+
+    public void setUI() {
+        answerSpinner = (Spinner) findViewById(R.id.answer_day);
+    }
+
+    private void createSpinnerData() {
+
+        answerArray.add("วันอาทิตย์");
+        answerArray.add("วันจันทร์");
+        answerArray.add("วันอังคาร");
+        answerArray.add("วันพุธ");
+        answerArray.add("วันพฤหัสบดี");
+        answerArray.add("วันศุกร์");
+        answerArray.add("วันเสาร์");
+
+    }
+}
