5fa269ab667e024a44c71ac4e2d55eb5e31aa151
Pannawat Nokket <nogket@gmail.com>
2018-02-07 08:10:26 +0700

    set loading animation login

--- app/src/main/java/com/custu/project/walktogether/LoginActivity.java
+++ app/src/main/java/com/custu/project/walktogether/LoginActivity.java
@@ -1,10 +1,16 @@
 package com.custu.project.walktogether;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -20,6 +26,8 @@ import com.custu.project.walktogether.util.ErrorDialog;
 import com.custu.project.walktogether.util.UserManager;
 import com.google.gson.JsonObject;
 
+import br.com.simplepass.loading_button_lib.customViews.CircularProgressButton;
+import br.com.simplepass.loading_button_lib.interfaces.OnAnimationEndListener;
 import okhttp3.ResponseBody;
 import retrofit2.Retrofit;
 
@@ -29,6 +37,8 @@ public class LoginActivity extends Activity implements BasicActivity, View.OnCli
     private EditText username;
     private EditText password;
     private ProgressDialog progressDialog;
+    private CircularProgressButton circularProgressButton;
+
 
     private Caretaker caretaker;
 
@@ -49,6 +59,7 @@ public class LoginActivity extends Activity implements BasicActivity, View.OnCli
     private void setListener() {
         registerBtn.setOnClickListener(this);
         loginBtn.setOnClickListener(this);
+        circularProgressButton.setOnClickListener(this);
 
     }
 
@@ -63,6 +74,18 @@ public class LoginActivity extends Activity implements BasicActivity, View.OnCli
             case R.id.login: {
                 if (validate()) {
                     login();
+                }
+            }
+            case R.id.sign_in: {
+                if (validate()) {
+                    circularProgressButton.startAnimation();
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            // TODO Auto-generated method stub
+                            login();
+                        }
+                    }, 2000);
                 }
             }
 
@@ -82,14 +105,13 @@ public class LoginActivity extends Activity implements BasicActivity, View.OnCli
     }
 
     public void login() {
-        progressDialog.show();
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("userName", username.getText().toString().trim());
         jsonObject.addProperty("password", password.getText().toString().trim());
 
         ConnectServer.getInstance().post(new OnDataSuccessListener() {
             @Override
-            public void onResponse(JsonObject object, Retrofit retrofit) {
+            public void onResponse(final JsonObject object, Retrofit retrofit) {
                 if (object != null) {
                     int status = object.get("status").getAsInt();
                     if (status == 200) {
@@ -98,15 +120,49 @@ public class LoginActivity extends Activity implements BasicActivity, View.OnCli
                             Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
                             startActivity(intent);
                         } else {
-                            caretaker = CaretakerModel.getInstance().getCaretaker(object);
-                            UserManager.getInstance(LoginActivity.this).storeCaretaker(caretaker);
-                            Intent intent = new Intent(LoginActivity.this, HomeCaretakerActivity.class);
-                            startActivity(intent);
+                            circularProgressButton.revertAnimation(new OnAnimationEndListener() {
+                                @SuppressLint("ResourceAsColor")
+                                @Override
+                                public void onAnimationEnd() {
+                                    circularProgressButton.setText("เข้าสู่ระบบสำเร็จ");
+                                    circularProgressButton.setTextColor(Color.parseColor("#FFFFFF"));
+                                    circularProgressButton.setBackgroundResource(R.drawable.shapebutton_complete);
+                                    new Handler().postDelayed(new Runnable() {
+                                        @Override
+                                        public void run() {
+                                            caretaker = CaretakerModel.getInstance().getCaretaker(object);
+                                            UserManager.getInstance(LoginActivity.this).storeCaretaker(caretaker);
+                                            Intent intent = new Intent(LoginActivity.this, HomeCaretakerActivity.class);
+                                            startActivity(intent);
+                                        }
+                                    }, 1000);
+                                }
+                            });
                         }
 
                     } else {
+                        circularProgressButton.revertAnimation(new OnAnimationEndListener() {
+                            @RequiresApi(api = Build.VERSION_CODES.M)
+                            @SuppressLint("ResourceAsColor")
+                            @Override
+                            public void onAnimationEnd() {
+                                circularProgressButton.setText(object.get("message").getAsString());
+                                circularProgressButton.setTextColor(Color.parseColor("#FFFFFF"));
+                                circularProgressButton.setBackgroundResource(R.drawable.shapebutton_error);
+                                new Handler().postDelayed(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        circularProgressButton.startAnimation();
+                                        circularProgressButton.revertAnimation();
+                                        circularProgressButton.setText("เข้าสู่ระบบ");
+                                        circularProgressButton.setTextColor(Color.parseColor("#3F51B5"));
+                                        circularProgressButton.setBackgroundResource(R.drawable.shapebutton_normal);
+                                    }
+                                }, 2000);
+                            }
+                        });
+
                         progressDialog.dismiss();
-                        ErrorDialog.getInstance().showDialog(LoginActivity.this, object.get("message").getAsString());
                     }
                 }
             }
@@ -135,7 +191,8 @@ public class LoginActivity extends Activity implements BasicActivity, View.OnCli
         loginBtn = (Button) findViewById(R.id.login);
         username = (EditText) findViewById(R.id.input_username);
         password = (EditText) findViewById(R.id.input_password);
-
+        circularProgressButton = (CircularProgressButton) findViewById(R.id.sign_in);
+        circularProgressButton.setBackgroundResource(R.drawable.shapebutton);
     }
 
     @Override
@@ -153,6 +210,7 @@ public class LoginActivity extends Activity implements BasicActivity, View.OnCli
     @Override
     public void onDestroy() {
         super.onDestroy();
+        circularProgressButton.dispose();
         if (progressDialog != null && progressDialog.isShowing()) {
             progressDialog.cancel();
         }
