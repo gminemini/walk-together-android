e2d8eab882459420712061941743846746a32a0e
Your Name <mine_wongteeravit@hotmail.com>
2018-02-15 12:56:26 +0700

    check permission

--- app/src/main/java/com/custu/project/walktogether/QrCodeActivity.java
+++ app/src/main/java/com/custu/project/walktogether/QrCodeActivity.java
@@ -10,7 +10,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Toast;
 
-import com.custu.project.project.walktogether.R;
 import com.google.zxing.Result;
 
 import me.dm7.barcodescanner.zxing.ZXingScannerView;
@@ -18,6 +17,7 @@ import me.dm7.barcodescanner.zxing.ZXingScannerView;
 public class QrCodeActivity extends AppCompatActivity implements ZXingScannerView.ResultHandler {
     private ZXingScannerView mScannerView;
     private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int REQUEST_CAMERARESULT=201;
 
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
@@ -25,9 +25,15 @@ public class QrCodeActivity extends AppCompatActivity implements ZXingScannerVie
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
 
-        if (checkSelfPermission(Manifest.permission.CAMERA)
-                != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if(this.checkSelfPermission(Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED){
+                ///method to get Images
+            }else{
+                if(shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)){
+                    Toast.makeText(this,"Your Permission is needed to get access the camera",Toast.LENGTH_LONG).show();
+                }
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE,Manifest.permission.CAMERA}, REQUEST_CAMERARESULT);
+            }
         }
 
         mScannerView = new ZXingScannerView(this);
