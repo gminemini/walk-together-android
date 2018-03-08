7b9ebeeaff52bc1ac96ecacb612c0748aae87668
Pannawat Nokket <nogket@gmail.com>
2018-03-09 02:38:29 +0700

    wip

--- app/src/main/java/com/custu/project/walktogether/ProfileActivity.java
+++ app/src/main/java/com/custu/project/walktogether/ProfileActivity.java
@@ -1,15 +1,11 @@
 package com.custu.project.walktogether;
 
-import android.app.Dialog;
-import android.content.Context;
 import android.content.Intent;
-import android.graphics.drawable.ColorDrawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.custu.project.project.walktogether.R;
