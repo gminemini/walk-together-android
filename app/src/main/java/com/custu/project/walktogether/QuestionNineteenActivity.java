4330dcf59f8a490812119eb4b5b0bc14b5689134
Pannawat Nokket <nogket@gmail.com>
2018-03-08 15:43:20 +0700

    wip

--- app/src/main/java/com/custu/project/walktogether/QuestionNineteenActivity.java
+++ app/src/main/java/com/custu/project/walktogether/QuestionNineteenActivity.java
@@ -151,7 +151,7 @@ public class QuestionNineteenActivity extends AppCompatActivity implements Basic
                     boolean isPass = object.getAsJsonObject("data").get("isPass").getAsBoolean();
                     if (isPass) {
                         Intent intent = new Intent(QuestionNineteenActivity.this, ResultPassActivity.class);
-                        intent.putExtra("idPatient", object.getAsJsonObject("data").get("idPatient").getAsLong());
+                        intent.putExtra("idPatient", object.getAsJsonObject("data").get("idPatient").getAsInt());
                         startActivity(intent);
                     } else {
                         int score = object.getAsJsonObject("data").get("score").getAsInt();
