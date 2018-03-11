03a65dd3cf47afc890d52109e10612f83d50644a
Pannawat Nokket <nogket@gmail.com>
2018-03-11 23:33:39 +0700

    wip

---
+++ app/src/main/java/com/custu/project/walktogether/EditCaretakerProfileActivity.java
@@ -0,0 +1,15 @@
+package com.custu.project.walktogether;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.custu.project.project.walktogether.R;
+
+public class EditCaretakerProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_caretaker_profile);
+    }
+}
