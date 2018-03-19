3f75ee6f12551f30057bcd62a292a01bd759977a
Pannawat Nokket <nogket@gmail.com>
2018-03-20 03:13:09 +0700

    add logg

--- app/src/main/java/com/custu/project/walktogether/fcm/MyFirebaseMessagingService.java
+++ app/src/main/java/com/custu/project/walktogether/fcm/MyFirebaseMessagingService.java
@@ -42,49 +42,8 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
             sendNotification(data);
     }
 
-    private void sendNotificationTest(Map<String, String> data) {
-        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
-
-        Intent intent = new Intent(this, SplashScreenActivity.class);
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_ONE_SHOT);
-
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this, "channel_id")
-                .setContentTitle(data.get("title"))
-                .setContentText(data.get("body"))
-                .setAutoCancel(true)
-                .setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION))
-                .setContentIntent(pendingIntent)
-                .setContentInfo(data.get("title"))
-                .setLargeIcon(icon)
-                .setColor(Color.RED)
-                .setLights(Color.RED, 1000, 300)
-                .setDefaults(Notification.DEFAULT_VIBRATE)
-                .setSmallIcon(R.mipmap.ic_launcher);
-
-        NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-
-        // Notification Channel is required for Android O and above
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            NotificationChannel channel = new NotificationChannel(
-                    "channel_id", "channel_name", NotificationManager.IMPORTANCE_DEFAULT
-            );
-            channel.setDescription("channel description");
-            channel.setShowBadge(true);
-            channel.canShowBadge();
-            channel.enableLights(true);
-            channel.setLightColor(Color.RED);
-            channel.enableVibration(true);
-            channel.setVibrationPattern(new long[]{100, 200, 300, 400, 500});
-            notificationManager.createNotificationChannel(channel);
-        }
-
-        notificationManager.notify(0, notificationBuilder.build());
-    }
-
-
     private void sendNotification(Map<String, String> data) {
-        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher);
+        Bitmap icon = BitmapFactory.decodeResource(getResources(), R.mipmap.ic_launcher_foreground);
         Intent intent;
         if (UserManager.getInstance(this).getCaretaker()!=null)
             intent = new Intent(this, ReHomeCaretakerActivity.class);
@@ -105,7 +64,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
                 .setLargeIcon(icon)
                 .setColor(Color.RED)
                 .setDefaults(Notification.DEFAULT_VIBRATE)
-                .setSmallIcon(R.mipmap.ic_launcher);
+                .setSmallIcon(R.mipmap.ic_launcher_foreground);
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
 
