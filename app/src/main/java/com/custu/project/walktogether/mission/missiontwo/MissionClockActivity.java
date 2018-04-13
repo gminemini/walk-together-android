48a8dcbdd7ab56d70a4bea7a3149c7d8b3447312
Your Name <mine_wongteeravit@hotmail.com>
2018-04-13 17:01:37 +0700

    finish activity topic2 (clock)

--- app/src/main/java/com/custu/project/walktogether/mission/missiontwo/MissionClockActivity.java
+++ app/src/main/java/com/custu/project/walktogether/mission/missiontwo/MissionClockActivity.java
@@ -6,16 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
+import android.widget.Spinner;
 
 import com.custu.project.project.walktogether.R;
 import com.custu.project.walktogether.util.BasicActivity;
 import com.custu.project.walktogether.util.ConfigService;
 
+import java.util.ArrayList;
+
 public class MissionClockActivity extends AppCompatActivity implements BasicActivity, View.OnClickListener{
+    private Spinner answerSpinner_hour;
+    private Spinner answerSpinner_minus;
+    private ArrayList<String> answerArray_hour = new ArrayList<String>();
+    private ArrayList<String> answerArray_minus = new ArrayList<String>();
 
     private EditText inputMision;
     private ImageView imageQuestion;
@@ -32,6 +40,11 @@ public class MissionClockActivity extends AppCompatActivity implements BasicActi
         setUI();
         setListener();
         countDownTime();
+        createSpinnerData();
+        ArrayAdapter<String> adapterArray_hour = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, answerArray_hour);
+        ArrayAdapter<String> adapterArray_minus = new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, answerArray_minus);
+        answerSpinner_hour.setAdapter(adapterArray_hour);
+        answerSpinner_minus.setAdapter(adapterArray_minus);
 
     }
     private void countDownTime() {
@@ -100,6 +113,9 @@ public class MissionClockActivity extends AppCompatActivity implements BasicActi
 
     @Override
     public void setUI() {
+        answerSpinner_hour = (Spinner) findViewById(R.id.input_hour);
+        answerSpinner_minus = (Spinner) findViewById(R.id.input_minus);
+
         nextBtn = (Button) findViewById(R.id.next);
         inputMision = findViewById(R.id.input_missionfive);
         imageQuestion = findViewById(R.id.image);
@@ -116,6 +132,40 @@ public class MissionClockActivity extends AppCompatActivity implements BasicActi
 
     @Override
     public void initProgressDialog() {
+
+    }
+    private void createSpinnerData() {
+        answerArray_hour.add("เลือกคำตอบ");
+        answerArray_minus.add("เลือกคำตอบ");
+        answerArray_hour.add("0");
+        answerArray_hour.add("1");
+        answerArray_hour.add("2");
+        answerArray_hour.add("3");
+        answerArray_hour.add("4");
+        answerArray_hour.add("5");
+        answerArray_hour.add("6");
+        answerArray_hour.add("7");
+        answerArray_hour.add("8");
+        answerArray_hour.add("9");
+        answerArray_hour.add("10");
+        answerArray_hour.add("11");
+        answerArray_hour.add("12");
+        answerArray_hour.add("13");
+        answerArray_hour.add("14");
+        answerArray_hour.add("15");
+        answerArray_hour.add("16");
+        answerArray_hour.add("17");
+        answerArray_hour.add("18");
+        answerArray_hour.add("19");
+        answerArray_hour.add("20");
+        answerArray_hour.add("21");
+        answerArray_hour.add("22");
+        answerArray_hour.add("23");
+        answerArray_hour.add("24");
+
+        for (int i = 0 ;i<60;i++){
+            answerArray_minus.add(""+i+"");
+        }
 
     }
 }
