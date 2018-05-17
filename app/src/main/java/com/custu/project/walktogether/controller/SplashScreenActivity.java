415691c9af2f54aee0ea066cc972465dee23dcd5
Pannawat Nokket <nogket@gmail.com>
2018-05-18 06:01:46 +0700

    compare

--- app/src/main/java/com/custu/project/walktogether/controller/SplashScreenActivity.java
+++ app/src/main/java/com/custu/project/walktogether/controller/SplashScreenActivity.java
@@ -1,23 +1,34 @@
 package com.custu.project.walktogether.controller;
 
 import android.app.Activity;
-import android.content.Intent;
-import android.os.Handler;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.os.Handler;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.ProgressBar;
 
 import com.crashlytics.android.Crashlytics;
 import com.custu.project.project.walktogether.R;
-import com.custu.project.walktogether.controller.caretaker.ReHomeCaretakerActivity;
-import com.custu.project.walktogether.controller.patient.ReHomePatientActivity;
-import com.custu.project.walktogether.controller.patient.RegisterPatientActivity;
-import com.custu.project.walktogether.data.Patient;
-import com.custu.project.walktogether.util.UserManager;
+import com.custu.project.walktogether.util.lib.OpenCVUtil;
 import com.github.ybq.android.spinkit.style.ThreeBounce;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.core.MatOfFloat;
+import org.opencv.core.MatOfInt;
+import org.opencv.imgproc.Imgproc;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Collections;
+
 import io.fabric.sdk.android.Fabric;
+import org.opencv.android.Utils;
 
 public class SplashScreenActivity extends Activity {
     Handler handler;
@@ -25,8 +36,26 @@ public class SplashScreenActivity extends Activity {
     long delay_time;
     long time = 2000L;
 
+    private BaseLoaderCallback baseLoaderCallback = new BaseLoaderCallback(this) {
+        @Override
+        public void onManagerConnected(int status) {
+            switch (status) {
+                case LoaderCallbackInterface.SUCCESS: {
+
+                    compare();
+                }
+                break;
+                default: {
+                    super.onManagerConnected(status);
+                }
+                break;
+            }
+        }
+    };
+
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         Fabric.with(this, new Crashlytics());
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
@@ -36,7 +65,14 @@ public class SplashScreenActivity extends Activity {
         ThreeBounce threeBounce = new ThreeBounce();
         progressBar.setIndeterminateDrawable(threeBounce);
 
-        int splashInterval = 2000;
+        if (OpenCVLoader.initDebug()) {
+            Log.d("onManagerConnected: ", "onCreate: ");
+        } else {
+            Log.d("onManagerConnected: ", "onManagerConnected:");
+            OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_3_1_0, this, baseLoaderCallback);
+        }
+
+        /*int splashInterval = 2000;
         new Handler().postDelayed(() -> {
             Intent intent;
             if (UserManager.getInstance(SplashScreenActivity.this).getCaretaker() != null) {
@@ -58,7 +94,7 @@ public class SplashScreenActivity extends Activity {
                 startActivity(intent);
             }
             finish();
-        }, splashInterval);
+        }, splashInterval);*/
     }
 
     public void onResume() {
@@ -77,5 +113,42 @@ public class SplashScreenActivity extends Activity {
     @Override
     public void onBackPressed() {
     }
+
+    private void compare() {
+        Bitmap b1 = BitmapFactory.decodeResource(getResources(), R.drawable.test1);
+        Bitmap b2 = BitmapFactory.decodeResource(getResources(), R.drawable.test2);
+        compare_image(b1, b2);
+    }
+
+    public void compare_image(Bitmap img_1, Bitmap img_2)
+    {
+        Mat mat_1 = new Mat();
+        Mat mat_2 = new Mat();
+
+
+        Bitmap bitmap1 = img_1.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bitmap1, mat_1);
+
+        Bitmap bitmap2 = img_2.copy(Bitmap.Config.ARGB_8888, true);
+        Utils.bitmapToMat(bitmap2, mat_2);
+
+        Mat hist_1 = new Mat();
+        Mat hist_2 = new Mat();
+
+        MatOfFloat ranges = new MatOfFloat(0f,256f);
+        MatOfInt histSize = new MatOfInt(25);
+
+        Imgproc.calcHist(Collections.singletonList(mat_1), new MatOfInt(0),
+                new Mat(), hist_1, histSize, ranges);
+        Imgproc.calcHist(Collections.singletonList(mat_2), new MatOfInt(0),
+                new Mat(), hist_2, histSize, ranges);
+
+        double res = Imgproc.compareHist(hist_1, hist_2, Imgproc.CV_COMP_CORREL);
+        Double d = res * 100;
+        Log.d("compare_image: ", "compare_image: "+d.intValue());
+        Log.d("compare_image: ", "compare_image: "+hist_1.dump());
+        Log.d("compare_image: ", "compare_image: "+hist_2.dump());
+    }
+
 }
 
